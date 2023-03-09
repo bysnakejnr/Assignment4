a7974d39f9e91666ea7a01ca7b99d9759d59293f
bysnakejnr <barisberber.ca@gmail.com>
2023-03-09 14:14:40 -0500

    last commit

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 /*********************************************************************************
-* WEB700 – Assignment 03
+* WEB700 – Assignment 04
 * I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part
 * of this assignment has been copied manually or electronically from any other source
 * (including 3rd party web sites) or distributed to other students.
 *
-* Name: Baris Berber Student ID: 133731224 Date: 17 February 2023
+* Name: Baris Berber Student ID: 133731224 Date: 09 March 2023
+*
+* Online (Cyclic) Link: https://shy-jade-millipede-tam.cyclic.app/
 *
 ********************************************************************************/ 
 
